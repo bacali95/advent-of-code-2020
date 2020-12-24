988cc3dd7202e1942e0a23bec05531fe7d98fb4d
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2020-12-24 11:42:09 +0100

    Day 20

---
+++ src/days/Day20.scala
@@ -0,0 +1,11 @@
+package days
+
+case class Day20(data: String) extends DayX {
+  val list: List[String] = transformDataToArray[String](data, s => s)
+
+  def partOne(): Unit = {
+  }
+
+  def partTwo(): Unit = {
+  }
+}
